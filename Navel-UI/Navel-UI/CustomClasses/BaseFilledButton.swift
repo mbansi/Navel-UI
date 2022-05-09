43bbf3e4b44d973ebd1c38d64d642d02912db107
Bansi Mamtora <mbansi306@gmail.com>
2022-05-09 18:14:33 +0530

    Custom controls

---
+++ Navel-UI/Navel-UI/CustomClasses/BaseFilledButton.swift
@@ -0,0 +1,29 @@
+//
+//  CustomFilledButton.swift
+//  Navel-UI
+//
+//  Created by Bansi Mamtora on 30/04/22.
+//
+
+import UIKit
+
+class BaseFilledButton: UIButton {
+  
+    //MARK: - init
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: - Functions
+    func setup() {
+        self.titleLabel?.font = R.font.ralewaySemiBold(size: 12)
+        self.layer.cornerRadius = 23
+        self.titleLabel?.textColor = R.color.mediumGreen()
+        self.layer.backgroundColor = R.color.gold()?.cgColor
+    }
+}
